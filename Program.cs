d9c5c2ef5fb07e5fe2685f0656e9b14fdc90351a
Nemro100 <120989174+Nemro100@users.noreply.github.com>
2022-12-22 00:25:34 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System.Drawing;
+
+Console.WriteLine("enter number");
+double number = double.Parse(Console.ReadLine());
+
+
+for (double i = 1; i <= number; i++)
+{
+
+    
+    if (prime(i))
+    {
+       
+        Console.WriteLine(" prime number >>>>> {0} ",i);
+    }
+    
+}
+static bool prime(double n)
+{
+   
+    if (n == 1 || n == 0) return false ;
+
+    
+    for (double i = 2; i < n; i++)
+    {
+       
+        if (n % i == 0) return false;
+    }
+    
+    return true;
+}
+
+
